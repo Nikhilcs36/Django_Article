d693fa55bf71507614867e8bed0a9a5aa8a9ab2d
Nikhil cs <nikhilcs36@gmail.com>
2023-05-29 17:21:20 +0530

    Widgets in forms | Django | Python

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,15 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        # fields = "__all__"
-        fields = ("name","email","message")
-        exclude =('name',)
+        fields = "__all__"
+        # fields = ("name","email","message")
+        # exclude =('name',)
+        
+        widgets = {
+            "name": forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter name'}),
+            "email": forms.EmailInput(attrs={'class':'form-control', 'placeholder':'enter email'}),
+            "phone_number": forms.NumberInput(attrs={'class':'form-control', 'placeholder':'enter phone number'}),
+            "message": forms.Textarea(attrs={'class':'form-control', 'placeholder':'enter message'}),
+        }
+        
         
