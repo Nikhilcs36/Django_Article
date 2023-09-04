2b5b387ac72710197fc6808adacd9e1efd88874d
Nikhil cs <nikhilcs36@gmail.com>
2023-09-04 11:23:54 +0530

    Creating custom user registration form

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import SignupForm
+
+# def signup(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Your account was created successfully.")
+#             return redirect('home')
+#         else:
+#             messages.error(request, "Error")
+#     else:
+#         form = UserCreationForm()
+#     return render(request, "auth/signup.html", {'form': form})
 
 def signup(request):
-    return render(request,"auth/signup.html")
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account was created successfully.")
+            return redirect('home')
+        else:
+            messages.error(request, "Error")
+    else:
+        form = SignupForm()
+    return render(request, "auth/signup.html", {'form': form})
+        
 
 def login(request):
     return render(request,"auth/login.html")
